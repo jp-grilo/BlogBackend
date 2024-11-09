988686feb2becae10f39500054a7c8e27431476f
jp-grilo <joaoplopescg@gmail.com>
2024-11-09 18:38:08 -0300

    update na incrementação de id dos posts

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlogBackend.Models
 {
     public class Post
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        
         public string? Title { get; set; }
         public string? Content { get; set; }
     }
